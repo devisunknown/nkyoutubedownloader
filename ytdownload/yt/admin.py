1a6508f8c1258070e81b414fa1bba6ef539a8195
devisknown <starblegamer@gmail.com>
2026-07-09 16:16:07 +0000

    worked

--- ytdownload/yt/admin.py
+++ ytdownload/yt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import DownloadTicket
 
-# Register your models here.
+class DownloadTicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'video_url', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at', 'updated_at')
+    search_fields = ('id', 'user__username', 'video_url')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+admin.site.register(DownloadTicket, DownloadTicketAdmin)
